3ba0b1c3a1a4279a0bb0fde8ec90a81258f1bcbe
bharatraj1508 <bharat.raj1508@gmail.com>
2023-05-23 13:49:46 -0400

    updated routes

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,20 +1,17 @@
 require("./models/user");
-require("./models/Track");
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const authRoutes = require("./routes/authRoutes");
-const trackRoutes = require("./routes/trackRoutes");
 const requireAuth = require("./middlewares/requireAuth");
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(authRoutes);
-app.use(trackRoutes);
 
 const mongoUri =
-  "mongodb+srv://bharatraj07:Snappy150897@cluster0.6va19gp.mongodb.net/Track-app";
+  "mongodb+srv://bharatraj07:Snappy150897@cluster0.6va19gp.mongodb.net/Thoughts-BlogApp";
 mongoose.connect(mongoUri);
 
 mongoose.connection.on("connected", () => {
