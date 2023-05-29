14031f237e72e5410f55a616712499de60460362
Bharat Raj Verma <bharat.raj1508@gmail.com>
2023-05-29 19:37:10 -0400

    final changes to middleware

--- src/middlewares/checkProfile.js
+++ src/middlewares/checkProfile.js
@@ -3,9 +3,8 @@ const Profile = mongoose.model("UserProfile");
 
 const checkProfileExist = async (req, res, next) => {
   const userProfile = await Profile.findOne({ userId: req.user._id });
-  console.log(userProfile);
   if (!userProfile) {
-    return res.status(500).send({ error: "User must have profile first." });
+    return res.status(500).json({ error: "User must have profile first." });
   }
   next();
 };
